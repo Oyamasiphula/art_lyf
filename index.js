c1aa1025e7ff1f4bad11112821b6fbb0cc30c189
oyamasiphula <oyama@projectcodex.co>
2018-12-12 14:05:33 +0200

    added a new port number to suit it with heroku

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('about',function(req,res){
 
 })
 // <portSetup>port delcaration
-var port = process.env.port || 2000
+var port = process.env.port || 8000
 // </portSetup>
 
 // <serveCodeBlocksRun>Lets configure our localhost server's port
