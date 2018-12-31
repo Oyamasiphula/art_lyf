dc40a4b134d7fdaec144f5a395c6d4c823d4d6eb
Oyamasiphula <oyamasiphula@gmail.com>
2018-12-31 20:33:40 +0200

    added sendmail with arguments and the next callback function

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,3 +1,70 @@
 'use strict';
 
 const nodemailer = require('nodemailer');
+
+var sendMail = function(req, res, next) {
+  var input = JSON.parse(JSON.stringify(req.body));
+  var data = {
+    persons_name: input.name,
+    personal_message: input.message
+  };
+
+  async function main() {
+    // Generate SMTP service account from ethereal.email
+    let account = await nodemailer.createTestAccount();
+
+    console.log('Credentials obtained, sending message...');
+
+    // NB! Store the account object values somewhere if you want
+    // to re-use the same account for future mail deliveries
+
+    // Create a SMTP transporter object
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.USERNAME, // generated ethereal user
+        pass: process.env.PASS // generated ethereal password
+      },
+      logger: false,
+      debug: false // include SMTP traffic in the logs
+    }, {
+      // default message fields
+
+      // sender info
+      from: data.persons_name + '" 👻"' + process.env.FROMMAIL, // sender address
+      headers: {
+        'X-Laziness-level': 1000 // just an example header, no need to use this
+      }
+    });
+
+    // Message object
+    let message = {
+      to: process.env.MAILTOSENDTO, // list of receivers
+
+      // Subject of the message
+      subject: 'Art Life Clothing ✔',
+
+      // plaintext body
+      text: data.personal_message,
+
+      // HTML body
+      html: '<b>' + data.personal_message + '</b>'
+    };
+
+    let info = await transporter.sendMail(message);
+
+    console.log('Message sent successfully!');
+    console.log(nodemailer.getTestMessageUrl(info));
+
+    // only needed when using pooled connections
+    transporter.close();
+  }
+  main().catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
+  next();
+}
+module.exports.sendMail = sendMail;
